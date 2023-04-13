f20d081de66af80b15cd7e0a5a8e5b5bcb7c9118
zaxwebs <zifidigital@gmail.com>
2023-04-14 02:42:45 +0530

    temporarily removed Demo

---
+++ src/lib/components/Demo.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { P, Toggle } from 'flowbite-svelte';
+	let comparisonLogos = ['google', 'samsung', 'adobe', 'disney', 'facebook', 'netflix'];
+	let isTrimmed = true;
+</script>
+
+<div class="mx-auto max-w-4xl">
+	<div class="flex flex-col items-center">
+		<Toggle
+			on:change={() => {
+				isTrimmed = !isTrimmed;
+			}}
+			class="mb-4"
+			checked={isTrimmed}>TrimPNG</Toggle
+		>
+		<P class="mb-8 text-slate-500">
+			{#if isTrimmed}
+				No whitespace around images.
+			{:else}
+				Whitespace around images.
+			{/if}
+		</P>
+		<div class="grid grid-cols-2 md:grid-cols-6 gap-12">
+			{#each comparisonLogos as logo}
+				<div class:self-center={isTrimmed} class:self-start={!isTrimmed} class:border={!isTrimmed}>
+					<img
+						class="w-full"
+						src="images/comparison-logos/{logo}{isTrimmed ? '-trimmed' : ''}.png"
+						alt="logo"
+					/>
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,8 +28,6 @@
 	let files = [];
 	let processedFilesCount = 0;
 	let bordersEnabled = false;
-	let comparisonLogos = ['google', 'samsung', 'adobe', 'disney', 'facebook', 'netflix'];
-	let isTrimmed = true;
 
 	const handleFileInput = async (newFiles) => {
 		const length = files.length;
@@ -204,40 +202,6 @@
 				{/each}
 			</div>
 			<Button pill={true} size="xl" on:click={downloadImages}>Download Images</Button>
-		{:else}
-			<div class="mx-auto max-w-4xl">
-				<div class="flex flex-col items-center">
-					<Toggle
-						on:change={() => {
-							isTrimmed = !isTrimmed;
-						}}
-						class="mb-4"
-						checked={isTrimmed}>TrimPNG</Toggle
-					>
-					<P class="mb-8 text-slate-500">
-						{#if isTrimmed}
-							No whitespace around images.
-						{:else}
-							Whitespace around images.
-						{/if}
-					</P>
-					<div class="grid grid-cols-2 md:grid-cols-6 gap-12">
-						{#each comparisonLogos as logo}
-							<div
-								class:self-center={isTrimmed}
-								class:self-start={!isTrimmed}
-								class:border={!isTrimmed}
-							>
-								<img
-									class="w-full"
-									src="images/comparison-logos/{logo}{isTrimmed ? '-trimmed' : ''}.png"
-									alt="logo"
-								/>
-							</div>
-						{/each}
-					</div>
-				</div>
-			</div>
 		{/if}
 	</div>
 </div>
